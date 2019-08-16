dd0ba8b8e1d3e9d76262202ff1be872aeb585d47
strauzen <strauzen@gmail.com>
2019-08-17 06:29:02 +0800

    Added excersizes 141_113 related to the for loop

---
+++ src/141_113_1.cpp
@@ -0,0 +1,18 @@
+/**
+ * Excersize 1.9: Write a program that uses a for to sum the numbers from 50 to 100. 
+ **/
+
+#include <iostream>
+
+int main() {
+    int v1 = 50, v2 = 100, sum = 0;
+
+    for (int i = 0; i <= v2; i++) {
+        sum += i;
+        ++i;
+    }
+
+    std::cout << "The sum of " << v1 << " to " << v2 << " inclusive is: " << sum << std::endl;
+
+    return 0;
+}
---
+++ src/141_113_3.cpp
@@ -0,0 +1,16 @@
+/**
+ * Excersize 1.10: In addition to the ++ operator that adds 1 to its operand, there is a decrement
+ * operator (--) that subtracts 1. Use the decrement operator to write a for that prints the 
+ * numbers from ten down to zero. 
+ */
+
+#include <iostream>
+
+int main() {
+
+    for (int val = 10; val >= 0; val--) {
+        std::cout << val << " ";
+    }
+
+    return 0;
+}
