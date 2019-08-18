45e0eb9a1b3a378571a034c9d0c936c43a0f3f53
strauzen <strauzen@gmail.com>
2019-08-18 14:27:07 +0800

    Changed directories on a per chapter basis, added the final exersize of chapter 1

---
+++ src/ch01/152_123.cpp
@@ -0,0 +1,38 @@
+/**
+ * Exercise 1.23: Write a program that reads several transactions and counts
+ * how many transactions occur for each ISBN.
+ **/
+
+#include <iostream>
+#include "../include/Sales_item.h"
+
+int main () {
+    
+    Sales_item total;
+
+    std::cout << "Please enter a transaction: " << std::endl;
+
+    if (std::cin >> total) {
+        Sales_item current;
+        int count = 1;
+
+        while(std::cin >> current) {
+            if (total.isbn() == current.isbn())
+                ++count;
+            else {
+                std::cout << "The total number of " << total.isbn() << " transactions is: " << count << std::endl;
+                total = current;
+                count = 1;
+            }
+        }
+
+        std::cout << "The total number of transactions is: " << count << std::endl;
+
+        return 0;
+    }
+    else {
+        std::cout << "No data!" << std::endl;
+        return -1;
+    }
+
+}
