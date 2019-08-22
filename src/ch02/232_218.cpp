b3df6354900987ad26d20f0d4dac7271e417e658
strauzen <strauzen@gmail.com>
2019-08-22 05:41:49 +0200

    Renamed excersizes anf finished pointer excersizes

---
+++ src/ch02/232_218.cpp
@@ -0,0 +1,16 @@
+/** 
+ * Exercise 2.18: Write code to change the value of a pointer. Write code to
+ * change the value to which the pointer points.
+**/
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int number = 9;
+    int * p;
+    *p = number;
+    p = &number;
+
+    return 0;
+}
