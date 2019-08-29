2d5c3440fe695af4984c465bb29355fe121822ad
strauzen <strauzen@gmail.com>
2019-08-29 23:27:00 +0200

    Completed excersize 322_032

---
+++ src/ch03/322_032.cpp
@@ -0,0 +1,48 @@
+/**
+ * Exercise 3.2: Write a program to read the standard input a line at a time.
+ * Modify your program to read a word at a time.
+ **/
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+void readLine() {
+    string line;
+
+    while(getline(cin, line)) {
+        cout << line << endl;
+    }
+}
+
+void readWord() {
+    string word;
+
+    while(cin >> word) {
+        cout << word << endl;
+    }
+}
+
+int main() {
+    int option = 0;
+    
+    cout << "Do you want to read lines (1) or words (2): " << endl;
+    cin >> option;
+    cout << "Please type something:" << endl;
+
+
+    switch (option)
+    {
+    case 1:
+        readLine();
+        break;
+    case 2:
+        readWord();
+        break;
+    default:
+        cout << "No valid option selected, exiting program." << endl; 
+        break;
+    }
+
+    return 0;
+}
